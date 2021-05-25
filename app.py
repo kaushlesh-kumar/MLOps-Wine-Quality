f3910f14b7e93a3887d625e9c8d0f8fc62e43775
Abhishek Sengupta <senabhishek44@gmail.com>
2021-05-25 14:14:59 +0530

    Moved some modules from app.py to prediction.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template, request, jsonify
+from prediction_service import prediction
 import os
-import yaml
-import joblib
-import numpy as np
-#from prediction_service import prediction
 
-params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -20,48 +16,22 @@ def index():
     if request.method == "POST":
         try:
             if request.form:    #if request is coming from the webapp
-                dict_req = dict(request.form).values()
-                dict_req = [list(map(float, dict_req))]
-                #response = prediction.form_response(dict_req)
-                response = predict(dict_req)
+                dict_req = dict(request.form)
+                # dict_req = [list(map(float, dict_req))]
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
+            
             elif request.json:  #if request is from any api testing app (postman)
-                #response = prediction.api_response(request.json)
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!! Please try again!"}
+            # error = {"error": "Something went wrong!! Please try again!"}
             error = {"error": e}
-
-            return render_template("404.html", error=error)
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
-
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Please try again!"}
-        return error
 
 
 if __name__ == "__main__":
